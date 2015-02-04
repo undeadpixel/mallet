f11382465cdac39b37797ef437e7a7a7a3c88d4a
Samuel Miravet Verde <samuelmiver@gmail.com>
2015-02-04 12:11:08 +0100

    updated

--- input_parsers/seq_parser.py
+++ input_parsers/seq_parser.py
@@ -1,3 +1,5 @@
+# First we define two functions to iterate over the file
+
 def FASTA_iterator( fasta_filename ):
     """"A Generator function that reads a FASTA file.
     In each iteration, the function returns a
@@ -35,3 +37,44 @@ def raw_seq_iterator(seqs_filename):
         for line in seqs_file:
             line = line.strip()
             yield line
+
+    seqs_filename.close()
+
+# Process to detect if the file is FASTA or raw and generate the class:
+
+def sequence_generator(input_file):
+    """Detects the type of input: raw or FASTA file and it
+    generate Sequence classes for each sequence
+
+    """
+
+    with open(input_file, "r") as filehandle:
+        if ">" in filehandle:
+            for identifier, sequence in FASTA_iterator(input_file):
+                return Sequence(identifier, sequence)
+        else:
+            i = 1
+            for sequence in raw_seq_iterator(input_file):
+                return Sequence("seq "+i, sequence)
+                i += 1
+
+# Class definition:
+
+class Sequence:
+
+    def __init__(self, identifier, sequence):
+
+        self.__identifier = identifier
+        self.__sequence = sequence
+
+    def get_identifier(self):
+        return self.__identifier
+
+    def get_sequence(self):
+        return self.__sequence
+
+    def __len__(self):
+        return len(self.get_sequence())
+
+    def __getitem__(self, i):
+        return self.get_sequence()[int(i)]
