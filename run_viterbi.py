42c7ec483dfa9929881360bad0bc339f9fe1bffa
Josep Arús <josep@joseparus.com>
2015-02-19 17:37:33 +0100

    removed run viterbi

--- run_viterbi.py
+++
@@ -1,9 +0,0 @@
-import mallet.viterbi as viterbi
-import mallet.input.tgf_parser as tgf
-
-import mallet.sequence as seq
-
-hmm = tgf.parse("test/files/sample1.tgf")
-
-result = viterbi.viterbi(hmm, seq.Sequence("P1", "AAAAAAA"))
-print result
