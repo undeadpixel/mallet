441262d6330b289e7ba10197780b08d964bde4af
Samuel Miravet Verde <samuelmiver@gmail.com>
2015-02-24 11:46:39 +0100

    tests

--- current_site_donors_stats.py
+++ current_site_donors_stats.py
@@ -4,9 +4,11 @@ import mallet.input.tgf_parser as tgf
 import mallet.input.sequence_parser as seq_parser
 
 sequences = seq_parser.parse("files/sequences/context_real_donors.raw")
-hmm = tgf.parse("files/hmms/u1_binding_tgf/u1_complete.tgf")
+# hmm = tgf.parse("files/hmms/u1_binding_tgf/u1_complete.tgf")
 # hmm = tgf.parse("files/hmms/u1_binding_tgf/u1_2_8.tgf")
 # hmm = tgf.parse("files/hmms/u1_binding_tgf/u1_3_7.tgf")
+# hmm = tgf.parse("files/hmms/tia1_binding_tgf/tia1_binding.tgf")
+hmm = tgf.parse("files/hmms/tia1_binding_tgf/u1_tia1_binding.tgf")
 
 correct_state_path = seq.Sequence("State path", "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE234*67890IIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
 # correct_state_path = seq.Sequence("State path", "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE34*6789IIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
